031dceaa55089c707d0cc48275f17d8b113454da
trinhlehoang1801 <trinhlehoang1801@gmail.com>
2021-05-16 11:32:14 +0700

    Dua project len git hub

---
+++ _CreateProjectConfig.bat
@@ -0,0 +1,15 @@
+REM ==== HT32 Create Project Config File ====
+REM   1. Change the value as "Y" or "N" to enable/disable the project file copy/create of each toolchain.
+REM   2. Delete this file (_CreateProjectConfig.bat) to reset the setting.
+
+
+SET IDE_KEILv5=Y
+SET IDE_KEIL=N
+SET IDE_IARv8=N
+SET IDE_IAR=N
+SET IDE_SES=N
+SET IDE_GNU=N
+SET IDE_SGPP=N
+
+
+SET IC_NAME=52352
